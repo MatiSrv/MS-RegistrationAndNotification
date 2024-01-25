aea625546c7267f2c9f66a89cdaec3d13d401108
MatiSrv <matisiravegna04@gmail.com>
2024-01-25 01:41:50 -0300

    project done

---
+++ microservices-notification/src/main/java/com/project/microservicesnotification/events/EventType.java
@@ -0,0 +1,5 @@
+package com.project.microservicesnotification.events;
+
+public enum EventType {
+    CREATED, UPDATED, DELETED
+}
---
+++ microservices-notification/src/main/java/com/project/microservicesnotification/services/EmailSenderService.java
@@ -0,0 +1,26 @@
+package com.project.microservicesnotification.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailSenderService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendEmail(String toEmail) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("demomsbiz@gmail.com");
+        message.setTo(toEmail);
+        message.setText("Congratulations! You have successfully registered on the site. We're thrilled to have you on board.\r\n" + //
+                "Thank you for joining our community. Feel free to explore and enjoy the features of the site");
+        message.setSubject("Welcome to our site! Your Registration Was Successful!");
+
+        mailSender.send(message);
+
+
+    }
+}
